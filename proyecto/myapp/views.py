625b62d119c263a874d5991f51afecda72d1e33b
Elm3rFgr0a <benjapinto27@gmail.com>
2025-05-16 20:05:53 -0400

    Personal Terminado

--- proyecto/myapp/views.py
+++ proyecto/myapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
-from .models import Pasillo, Box, Estadobox
+from .models import Pasillo, Box, Estadobox, Medico, Especialidad
 from django.db import connection
 from django.db.models import Count, Q
 
@@ -33,6 +33,37 @@ def disponibilidad(request):
     pasillos = Pasillo.objects.prefetch_related('box_set__idestadobox').all()
     return render(request, 'boxes.html', {'pasillos': pasillos})
 
+def personal(request):
+    busqueda = request.GET.get('busqueda', '')
+    especialidad_id = request.GET.get('especialidad', '')
+    
+    medicos = Medico.objects.select_related('idespecialidad').all().order_by('apellidomedico')
+    
+    if busqueda:
+        medicos = medicos.filter(
+            Q(nombremedico__icontains=busqueda) | 
+            Q(apellidomedico__icontains=busqueda)
+        )
+    
+    if especialidad_id:
+        medicos = medicos.filter(idespecialidad__idespecialidad=especialidad_id)
+        especialidad_nombre = Especialidad.objects.get(
+            idespecialidad=especialidad_id
+        ).nombreespecialidad
+    else:
+        especialidad_nombre = None
+    
+    especialidades = Especialidad.objects.filter(
+        idespecialidad__in=Medico.objects.values('idespecialidad').distinct()
+    ).order_by('nombreespecialidad')
+    
+    return render(request, 'personal.html', {
+        'medicos': medicos,
+        'especialidades': especialidades,
+        'busqueda_actual': busqueda,
+        'especialidad_actual': especialidad_id,
+        'especialidad_nombre': especialidad_nombre
+    })
 
 def login(request):
     return render(request, 'login.html')
@@ -45,9 +76,6 @@ def panel_admin(request):
 
 def agenda(request):
     return render(request, 'agenda.html')
-
-def personal(request):
-    return render(request, 'personal.html')
 
 def dashboard(request):
     return render(request, 'dashboard.html')

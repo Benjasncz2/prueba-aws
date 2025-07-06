2e9ca4e3ccb59b258d734ccb58d96c658142b13e
Benjasncz2 <benjasncz@gmail.com>
2025-07-06 19:22:46 -0400

    la vaina de google

--- proyecto/vesperdevs/urls.py
+++ proyecto/vesperdevs/urls.py
@@ -15,18 +15,36 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from myapp import views 
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # 1) Ruta para el archivo de verificación de Google:
+    path(
+        'boxes/google0eab2047eed3db07a.html',
+        TemplateView.as_view(
+            template_name='google0eab2047eed3db07a.html',
+            content_type='text/html'
+        ),
+        name='google_site_verification'
+    ),
+
+    # 2) Tus rutas normales:
     path('', views.login),
+    path('login/', views.login, name='login'),
     path('boxes/', views.disponibilidad_boxes, name='disponibilidad_boxes'),
     path('panel/', views.panel_admin),
     path('dashboard/', views.dashboard),
     path('personal/', views.personal),
     path('agenda/', views.agenda),
     path('implementos/', views.implementos),
-    path('cambiar_estado/<int:implemento_id>/<int:box_id>/<int:nuevo_estado>/',views.cambiar_estado_implemento,name='cambiar_estado'),
-    path('box/<int:box_id>/', views.box_detail, name='box_detail')
+    path(
+        'cambiar_estado/<int:implemento_id>/<int:box_id>/<int:nuevo_estado>/',
+        views.cambiar_estado_implemento,
+        name='cambiar_estado'
+    ),
+    path('box/<int:box_id>/', views.box_detail, name='box_detail'),
 ]
